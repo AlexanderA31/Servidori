ba8c87062191116714b7b6d4e5900eaeb620bd79
AlexanderA31 <ufredoalegria@hotmail.com>
2025-11-11 21:00:05 -0500

    style: Add ArrayList import to AdminController and update Maven status files

--- src/main/java/es/ucm/fdi/iu/control/AdminController.java
+++ src/main/java/es/ucm/fdi/iu/control/AdminController.java
@@ -23,6 +23,7 @@ import java.net.SocketTimeoutException;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.ArrayList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
